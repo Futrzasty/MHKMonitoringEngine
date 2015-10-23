c793e1ad1499b4c5d3f6f41fb8b476b50c90d551
Grzegorz Wozniak <wozniak@mruczek.org>
2015-10-23 11:31:42 +0200

    API Mikrotik i prosty plik testowy

--- test7.php
+++ test7.php
@@ -1,1 +1,18 @@
-test
+<?php
+    require_once __DIR__.'/lib/Mikrotik-API/routeros_api.class.php';
+
+    $API = new RouterosAPI();
+    $API->debug = false;
+    if ($API->connect('192.168.71.13', 'APIuser', 'zawszeCocaCola')) {
+
+        $API->write('/system/resource/print');
+
+        $READ = $API->read(false);
+        $ARRAY = $API->parseResponse($READ);
+
+        $res = $ARRAY[0];
+
+        echo '<br/><br/><br/>'.$res["cpu-load"];
+
+        $API->disconnect();
+    }
