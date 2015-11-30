fedc0079f79456b8d6afdf7d1d6ffb47059f77a2
Grzegorz Wozniak <wozniak@mruczek.org>
2015-11-30 15:28:45 +0100

    Testy nowego wyglądu kratek tablicy

---
+++ test8.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Tytuł strony</title>
+    <style type="text/css">
+        body {
+            background-color: black;
+            color: white;
+            font-family: Helvetica;
+        }
+
+        .low_l {
+            width: 50%;
+            color: black;
+            font-weight: bold;
+            font-size: 11pt;
+            margin: 0 auto;
+
+
+            overflow: hidden;
+            float: left;
+        }
+
+        .low_r {
+            width: 50%;
+            color: black;
+            font-weight: bold;
+            font-size: 11pt;
+            margin: 0 auto;
+
+            overflow: hidden;
+
+        }
+
+        .ramka {
+            border: 1px solid white;
+            width: 200px;
+            font-size: 35pt;
+            text-align: center;
+            padding: 4px 2px;
+            margin: 0 3px;
+            color: black;
+            overflow: hidden;
+            white-space: nowrap;
+        }
+    </style>
+</head>
+<body>
+
+<div style="float: left;">Opis kratki
+    <div class="ramka" style="background-color: red;">
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+        <b>5.432</b>
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+    </div></div>
+<div style="float: left;">Opis kratki
+    <div class="ramka" style="background-color: red;">
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+        <b>5.432</b>
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+    </div></div>
+<div style="float: left;">Opis kratki
+    <div class="ramka" style="background-color: red;">
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+
+        <b>5.432</b>
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+    </div></div><div style="float: left;">Opis kratki
+    <div class="ramka" style="background-color: red;">
+        <div class="low_l">&nbsp;</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+        <b>5.432</b>
+        <div class="low_l" style="background-color: green;">12 d 5 h</div>
+        <div class="low_r" style="background-color: green;">75 %</div>
+    </div></div>
+
+
+
+</body>
+</html>
