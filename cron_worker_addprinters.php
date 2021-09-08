80868454e61303dd2812c71d4bfa6127cfee9ac5
Mruczek <wozniak@mruczek.org>
2021-09-08 23:17:14 +0200

    Dla printer_details: zmiana mysql na mysqli, lepsza analiza danych z SNMP, porządki w kodzie

--- cron_worker_addprinters.php
+++ cron_worker_addprinters.php
@@ -1,37 +1,37 @@
 <?php
-        error_reporting(E_ALL & ~E_NOTICE);
-        include_once ("functions.php");
+    error_reporting(E_ALL & ~E_NOTICE);
+    include_once ("functions.php");
 
-        mysql_connect('localhost', $db_user, $db_pass);
-        @mysql_select_db($db_name) or die("Nie udało się wybrać bazy danych");
+    $server = new mysqli('localhost', $db_user, $db_pass, $db_name);
+    if (mysqli_connect_error()) {
+        die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+    }
 
-        mysql_query("SET CHARACTER SET utf8");
-        mysql_query("SET NAMES utf8");
-        mysql_query("SET COLLATION utf8");
+    $server->query("SET CHARACTER SET utf8");
+    $server->query("SET NAMES utf8");
+    $server->query("SET COLLATION utf8");
 
 	snmp_set_valueretrieval(SNMP_VALUE_PLAIN);
 
-        $hosts = get_JSON_value('getPrinterList');
+    $hosts = get_JSON_value('getPrinterList');
 	
 	foreach ($hosts as $host) {
-                $result = mysql_query("SELECT community FROM printer_snmp WHERE host = \"$host\";");
-                $row = mysql_fetch_assoc($result);
-                $community = $row["community"];
+        $result = $server->query("SELECT community FROM printer_snmp WHERE host = \"$host\";");
+        $row = $result->fetch_assoc();
+        $community = $row["community"];
 
-                $result = mysql_query("SELECT id FROM printer_snmp_details WHERE host = \"$host\";");
+        $result = $server->query("SELECT id FROM printer_snmp_details WHERE host = \"$host\";");
 
 		//$info = get_JSON_value("getSwitchInfobyHostMaster", $host);
-                if (mysql_num_rows($result) == 0) {
+        if ($result->num_rows == 0) {
 			$names = snmp2_walk ($host , $community , ".1.3.6.1.2.1.43.11.1.1.6");
 			//$values = snmp2_walk ($host , $community , ".1.3.6.1.2.1.43.11.1.1.9");
 			$maxes = snmp2_walk ($host , $community , ".1.3.6.1.2.1.43.11.1.1.8");
 			foreach ($names as $i => $name) {
-				mysql_query("INSERT INTO printer_snmp_details (host, name, value_max) VALUES (\"$host\", \"$name\", \"$maxes[$i]\");");
+                $server->query("INSERT INTO printer_snmp_details (host, name, value_max) VALUES (\"$host\", \"$name\", \"$maxes[$i]\");");
 			}
                         //$result=mysql_query("SELECT host, id, value FROM switch_ping WHERE host = \"$host\";");
-                }
-                //$row = mysql_fetch_assoc($result);
-		//mysql_query("UPDATE switch_ping SET `alias` = \"$info->Alias\", `name` = \"$info->Name\", `impact` = \"$info->Impact\" WHERE `host` = \"$host\";");
         }
-
-?>
+        //$row = $result->fetch_assoc();
+		//$server->query("UPDATE switch_ping SET `alias` = \"$info->Alias\", `name` = \"$info->Name\", `impact` = \"$info->Impact\" WHERE `host` = \"$host\";");
+	}
--- cron_worker_printerdetails.php
+++ cron_worker_printerdetails.php
@@ -1,40 +1,47 @@
 <?php
-        error_reporting(E_ALL & ~E_NOTICE);
-        include_once ("functions.php");
+	error_reporting(E_ALL & ~E_NOTICE);
+	include_once ("functions.php");
 
-        mysql_connect('localhost', $db_user, $db_pass);
-        @mysql_select_db($db_name) or die("Nie udało się wybrać bazy danych");
+	$server = new mysqli('localhost', $db_user, $db_pass, $db_name);
+	if (mysqli_connect_error()) {
+		die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+	}
 
-        mysql_query("SET CHARACTER SET utf8");
-        mysql_query("SET NAMES utf8");
-        mysql_query("SET COLLATION utf8");
+	$server->query("SET CHARACTER SET utf8");
+	$server->query("SET NAMES utf8");
+	$server->query("SET COLLATION utf8");
 
 	snmp_set_valueretrieval(SNMP_VALUE_PLAIN);
 
-        $hosts = get_JSON_value('getPrinterList');
+    $hosts = get_JSON_value('getPrinterList');
 	
 	foreach ($hosts as $host) {
-		$result = mysql_query("SELECT community FROM printer_snmp WHERE host = \"$host\";");
-		$row = mysql_fetch_assoc($result);
+		$result = $server->query("SELECT community FROM printer_snmp WHERE host = \"$host\";");
+		$row = $result->fetch_assoc();
 		$community = $row["community"];
 
 		$values = snmp2_walk ($host, $community, ".1.3.6.1.2.1.43.11.1.1.9");
 		$names = snmp2_walk ($host, $community, ".1.3.6.1.2.1.43.11.1.1.6");
 		foreach ($names as $i => $name) {
-			$result = mysql_query("SELECT value_max FROM printer_snmp_details WHERE host = \"$host\" AND name = \"$name\";");
+			$result = $server->query("SELECT value_max FROM printer_snmp_details WHERE host = \"$host\" AND name = \"$name\";");
 
-			if (mysql_num_rows($result) != 0) {
-				$row = mysql_fetch_assoc($result);	
+			if ($result->num_rows != 0) {
+				$row = $result->fetch_assoc();
 				if (($values[$i] >= 0) && ($row["value_max"] > 0)) {
 					$value = ($values[$i]/$row["value_max"]) * 100;
 				}
+				else if (($values[$i] >= 0) && ($row["value_max"] <= 0))
+				{
+					$value = $values[$i];
+				}
 				else
+				{
 					$value = -1;
-				
-				mysql_query("UPDATE printer_snmp_details SET `value` = \"$value\", `value_raw` = \"$values[$i]\", `last_change`= CURRENT_TIMESTAMP WHERE `host` = \"$host\" AND name = \"$name\";");
+				}
+
+				$server->query("UPDATE printer_snmp_details SET `value` = \"$value\", `value_raw` = \"$values[$i]\", `last_change`= CURRENT_TIMESTAMP WHERE `host` = \"$host\" AND name = \"$name\";");
 
 			}
 		}
-        }
+	}
 
-?>
